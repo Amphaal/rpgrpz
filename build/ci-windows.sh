5c943f5a1f34e10dd5de6eebe32620887a342204
Guillaume VARA <guillaume.vara@gmail.com>
2020-02-18 07:43:53 +0100

    qsd

--- build/ci-windows.sh
+++ build/ci-windows.sh
@@ -2,5 +2,5 @@
 cd ..
 mkdir _genRelease
 cd _genRelease
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-lld-mingw64-win.cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-lld-mingw64-linux.cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 ninja package
---
+++ cmake/toolchains/clang-lld-mingw64-linux.cmake
@@ -0,0 +1,21 @@
+SET(CMAKE_SYSTEM_NAME "Windows")
+
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH  "$ENV{MINGW64_ROOT}")
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+SET (CMAKE_C_COMPILER             "clang")
+SET (CMAKE_C_FLAGS                "-fuse-ld=lld -Wall -Wno-incompatible-ms-struct -Wno-incompatible-pointer-types -Wno-macro-redefined")
+SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
+SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
+
+SET (CMAKE_CXX_COMPILER             "clang++")
+SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wall -Wno-incompatible-ms-struct -Wno-incompatible-pointer-types -Wno-macro-redefined")
+SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG")
+SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
