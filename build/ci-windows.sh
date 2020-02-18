aa2a9fbb45234db27efc0ee7763e2c92a6395432
Guillaume VARA <guillaume.vara@gmail.com>
2020-02-18 06:06:20 +0100

    w

--- build/ci-windows.sh
+++ build/ci-windows.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd ..
-git clone https://github.com/Amphaal/rpgrpz.git
 mkdir _genRelease
 cd _genRelease
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/clang-lld-mingw64-win.cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
