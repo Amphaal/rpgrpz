cd28b57754c6c96a7fd41aeb32e236f29f3a9b7c
Amphaal <guillaume.vara@gmail.com>
2020-02-23 11:20:25 +0100

    wip

--- cmake/toolchains/msys.cmake
+++ cmake/toolchains/msys.cmake
@@ -1,3 +1,17 @@
+if(NOT DEFINED ENV{MINGW64_ROOT})
+    message(FATAL_ERROR "Required ENV variable MINGW64_ROOT does not exists. Please check README.md for more details !")
+endif()
+
+list(APPEND CMAKE_PREFIX_PATH 
+    $ENV{MINGW64_ROOT} 
+    $ENV{MINGW64_ROOT}/x86_64-w64-mingw32
+)
+
+#.dll are in bin folder
+list(APPEND CMAKE_LIBRARY_PATH 
+    $ENV{MINGW64_ROOT}/bin
+)
+
 SET (CMAKE_C_COMPILER             "clang")
 SET (CMAKE_C_FLAGS                "-fuse-ld=lld -Wall -Wno-incompatible-ms-struct -Wno-incompatible-pointer-types -Wno-macro-redefined")
 SET (CMAKE_C_FLAGS_DEBUG          "-O0 -g")
