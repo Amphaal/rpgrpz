7648ac378e70c13afe1d1ce76d9bcfdb3431d241
Guillaume VARA <guillaume.vara@gmail.com>
2020-02-25 06:29:21 +0100

    qqqq

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -1,2 +1,16 @@
 SET(CMAKE_LIBRARY_PATH ${CMAKE_SYSTEM_LIBRARY_PATH}) #must be defined for peldd
 SET(CMAKE_BUILD_TYPE RelWithDebInfo)
+
+SET (CMAKE_C_COMPILER             "x86_64-w64-mingw32-clang")
+SET (CMAKE_C_FLAGS                "-fuse-ld=lld -Wall -Wno-incompatible-ms-struct -Wno-incompatible-pointer-types -Wno-macro-redefined")
+SET (CMAKE_C_FLAGS_DEBUG          "-O0 -g")
+SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
+SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
+SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
+
+SET (CMAKE_CXX_COMPILER             "x86_64-w64-mingw32-clang++")
+SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wall -Wno-incompatible-ms-struct -Wno-incompatible-pointer-types -Wno-macro-redefined")
+SET (CMAKE_CXX_FLAGS_DEBUG          "-O0 -g")
+SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
+SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG")
+SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
