2f5d598560b8111bcd84596e36e59e5eb668da62
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-26 13:25:03 +0100

    qsdqsds

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -2,7 +2,7 @@ SET(CMAKE_SYSTEM_NAME "Windows")
 SET(CMAKE_SYSTEM_VERSION 6.1 CACHE INTERNAL "") #target Windows 7
 
 # search for programs in the build host directories
-set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 
 # for libraries and headers in the target directories
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
