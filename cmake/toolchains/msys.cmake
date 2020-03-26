30e733e7d8439b6088188d97b75b7997acce027b
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-26 11:33:27 +0100

    quick fixes

--- cmake/toolchains/msys.cmake
+++ cmake/toolchains/msys.cmake
@@ -11,14 +11,14 @@ list(APPEND CMAKE_PREFIX_PATH
 SET(CMAKE_LIBRARY_PATH $ENV{MINGW64_ROOT}/bin) #must be defined for peldd
 
 SET (CMAKE_C_COMPILER             "clang")
-SET (CMAKE_C_FLAGS                "-fuse-ld=lld -Wno-incompatible-ms-struct")
+SET (CMAKE_C_FLAGS                "-fuse-ld=lld")
 SET (CMAKE_C_FLAGS_DEBUG          "-O0 -g")
 SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
 SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
 SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
 
 SET (CMAKE_CXX_COMPILER             "clang++")
-SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld -Wno-incompatible-ms-struct")
+SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld")
 SET (CMAKE_CXX_FLAGS_DEBUG          "-O0 -g")
 SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
 SET (CMAKE_CXX_FLAGS_RELEASE        "-O4 -DNDEBUG")
