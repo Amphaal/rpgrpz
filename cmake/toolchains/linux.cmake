d5fc6efcec953db7accd700aff18619fb395d6f2
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-27 12:08:15 +0100

    qsdqsd

--- cmake/toolchains/linux.cmake
+++ cmake/toolchains/linux.cmake
@@ -2,9 +2,9 @@
 SET(CMAKE_BUILD_TYPE RelWithDebInfo)
 
 SET (CMAKE_C_COMPILER             "clang")
-SET (CMAKE_C_FLAGS                "-fuse-ld=lld")
+SET (CMAKE_C_FLAGS                "-Wno-unused-parameter")
 SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
 
 SET (CMAKE_CXX_COMPILER             "clang++")
-SET (CMAKE_CXX_FLAGS                "-fuse-ld=lld")
+SET (CMAKE_CXX_FLAGS                "-Wno-unused-parameter")
 SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
