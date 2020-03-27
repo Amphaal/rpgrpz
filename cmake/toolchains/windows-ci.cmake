e802d0232a07355c24eb152d6ff19ea446a002b5
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-27 18:11:43 +0100

    sdfdsf

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -1,6 +1,5 @@
 SET(CMAKE_SYSTEM_NAME "Windows")
 SET(CMAKE_SYSTEM_VERSION 6.1 CACHE INTERNAL "") #target Windows 7
-SET(CMAKE_CROSSCOMPILING_EMULATOR "wine")
 SET(MINGW64_ROOT "/mingw64")
 
 # search for programs in the build host directories
