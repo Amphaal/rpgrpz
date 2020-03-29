3d40586c24d229a3d712fac420c5fe4ebd96f92e
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-29 01:02:52 +0100

    dfdfd

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -18,15 +18,6 @@ list(APPEND CMAKE_PREFIX_PATH
 #.dll are in bin folder
 SET(MINGW_DLL_PATH ${MINGW64_ROOT}/bin)
 
-SET(XCOMPIL_STANDARD_INCLUDE_DIRECTORIES 
-    ${MINGW64_ROOT}/include
-    ${MINGW64_ROOT}/x86_64-w64-mingw32/include
-)
-SET(XCOMPIL_STANDARD_LIBRARIES
-    ${MINGW64_ROOT}/lib
-    ${MINGW64_ROOT}/x86_64-w64-mingw32/lib
-)
-
 #We want Release with debug infos
 SET(CMAKE_BUILD_TYPE RelWithDebInfo)
 
@@ -34,13 +25,9 @@ SET (CMAKE_ASM_MASM_COMPILER                "${CMAKE_SOURCE_DIR}/cmake/wrappers/
 SET (CMAKE_RC_COMPILER                      "${CMAKE_SOURCE_DIR}/cmake/wrappers/xwindres.sh")
 
 SET (CMAKE_C_COMPILER                       "clang")
-SET (CMAKE_C_LINKER_FLAGS_INIT              "-fuse-ld=lld -L ${MINGW64_ROOT}/include -L ${MINGW64_ROOT}/x86_64-w64-mingw32/lib")
-SET (CMAKE_C_FLAGS                          "-target x86_64-w64-mingw32")
+SET (CMAKE_C_FLAGS                          "-target x86_64-w64-mingw32 -fuse-ld=lld -L ${MINGW64_ROOT}/include -L ${MINGW64_ROOT}/x86_64-w64-mingw32/lib")
 SET (CMAKE_C_FLAGS_RELWITHDEBINFO           "-O2 -g")
 
 SET (CMAKE_CXX_COMPILER                     "clang++")
-SET (CMAKE_CXX_LINKER_FLAGS_INIT            "-fuse-ld=lld")
-SET (CMAKE_CXX_FLAGS                        "-target x86_64-w64-mingw32")
+SET (CMAKE_CXX_FLAGS                        "-target x86_64-w64-mingw32 -fuse-ld=lld -L ${MINGW64_ROOT}/include -L ${MINGW64_ROOT}/x86_64-w64-mingw32/lib")
 SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO         "-O2 -g")
-SET (CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${XCOMPIL_STANDARD_INCLUDE_DIRECTORIES})
-SET (CMAKE_CXX_STANDARD_LIBRARIES_INIT      ${XCOMPIL_STANDARD_LIBRARIES})
