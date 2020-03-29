7ca561ad4cfef2a023b434f66bfc31f103611416
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-29 01:05:34 +0100

    dfdf

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -25,7 +25,8 @@ SET (CMAKE_ASM_MASM_COMPILER                "${CMAKE_SOURCE_DIR}/cmake/wrappers/
 SET (CMAKE_RC_COMPILER                      "${CMAKE_SOURCE_DIR}/cmake/wrappers/xwindres.sh")
 
 SET (CMAKE_C_COMPILER                       "clang")
-SET (CMAKE_C_FLAGS                          "-target x86_64-w64-mingw32 -fuse-ld=lld -L ${MINGW64_ROOT}/include -L ${MINGW64_ROOT}/x86_64-w64-mingw32/lib")
+SET (CMAKE_C_FLAGS                          "-target x86_64-w64-mingw32 -fuse-ld=lld")
+SET (CMAKE_EXE_LINKER_FLAGS_INIT            "-L ${MINGW64_ROOT}/include -L ${MINGW64_ROOT}/x86_64-w64-mingw32/lib")
 SET (CMAKE_C_FLAGS_RELWITHDEBINFO           "-O2 -g")
 
 SET (CMAKE_CXX_COMPILER                     "clang++")
