35af2d94eb305d4d63af3deb6053e4b94483fd3a
Guillaume VARA <guillaume.vara@gmail.com>
2020-03-30 17:11:47 +0200

    update ci config

--- cmake/toolchains/linux-ci.cmake
+++ cmake/toolchains/linux-ci.cmake
@@ -1,8 +1,5 @@
 #We want Release with debug infos
-SET(CMAKE_BUILD_TYPE RelWithDebInfo)
+SET(CMAKE_BUILD_TYPE Release)
 
 SET (CMAKE_C_COMPILER             "clang")
-SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
-
 SET (CMAKE_CXX_COMPILER             "clang++")
-SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
