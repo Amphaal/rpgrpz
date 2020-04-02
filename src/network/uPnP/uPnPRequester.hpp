dafd672bbc0eb5236a3f4061f2cdd39f74d80d39
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-02 14:29:05 +0200

    Apply GPLv3

--- src/network/uPnP/uPnPRequester.hpp
+++ src/network/uPnP/uPnPRequester.hpp
@@ -1,3 +1,24 @@
+// RPG-RPZ
+// A simple Pen & Paper experience
+// Copyright (C) 2019-2020 Guillaume Vara
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+/* $Id: upnpc.c,v 1.119 2018/03/13 23:34:46 nanard Exp $ */
+/* Project : miniupnp
+ * Author : Thomas Bernard
+ * Copyright (c) 2005-2018 Thomas Bernard
+ * This software is subject to the conditions detailed in the
+ * LICENCE file provided in this distribution. */
+
 #pragma once
 
 #include "uPnPThread.hpp"
