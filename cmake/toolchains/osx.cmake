ec3608b7275c625085efe0877aa72dfa2effdfa0
Amphaal <guillaume.vara@gmail.com>
2020-04-10 04:07:42 +0200

    wip

--- cmake/toolchains/osx.cmake
+++ cmake/toolchains/osx.cmake
@@ -1,6 +1,8 @@
-SET(CMAKE_OSX_DEPLOYMENT_TARGET 10.13)
 SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON) #prevent linking errors once app shared
 SET(CMAKE_MACOSX_RPATH ON)
+
+#set(CMAKE_OSX_SYSROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk)
+SET(CMAKE_OSX_DEPLOYMENT_TARGET 10.13)
 
 list(APPEND CMAKE_PREFIX_PATH 
     "/usr/local/opt"
