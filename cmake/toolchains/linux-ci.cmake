88366276c39b802e1b02ad02b81806290f094ab0
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-13 14:26:55 +0200

    update

--- cmake/toolchains/linux-ci.cmake
+++ cmake/toolchains/linux-ci.cmake
@@ -1,10 +1,7 @@
 #We want Release with debug infos
 SET(CMAKE_BUILD_TYPE Release)
 
-list(APPEND CMAKE_PREFIX_PATH 
-    usr/
-    usr64/
-)
+SET(Qt5Multimedia_DIR "usr/lib/cmake/Qt5Multimedia")
 
 SET (CMAKE_C_COMPILER             "clang")
 SET (CMAKE_CXX_COMPILER             "clang++")
