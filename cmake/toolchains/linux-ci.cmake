8caab6349a97faaa56686dbd3b613c52bacf9ab4
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-13 14:29:19 +0200

    try for linux

--- cmake/toolchains/linux-ci.cmake
+++ cmake/toolchains/linux-ci.cmake
@@ -1,7 +1,7 @@
 #We want Release with debug infos
 SET(CMAKE_BUILD_TYPE Release)
 
-SET(Qt5Multimedia_DIR "usr/lib/cmake/Qt5Multimedia")
+SET(Qt5Multimedia_DIR   "usr/lib/cmake/Qt5Multimedia")
 
-SET (CMAKE_C_COMPILER             "clang")
-SET (CMAKE_CXX_COMPILER             "clang++")
+SET (CMAKE_C_COMPILER   "clang")
+SET (CMAKE_CXX_COMPILER "clang++")
