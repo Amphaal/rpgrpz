9d18db9afafb1a1d36a1ed2cfa0673c0eb537134
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-13 14:25:29 +0200

    update linux ci

--- cmake/toolchains/linux-ci.cmake
+++ cmake/toolchains/linux-ci.cmake
@@ -1,5 +1,10 @@
 #We want Release with debug infos
 SET(CMAKE_BUILD_TYPE Release)
 
+list(APPEND CMAKE_PREFIX_PATH 
+    usr/
+    usr64/
+)
+
 SET (CMAKE_C_COMPILER             "clang")
 SET (CMAKE_CXX_COMPILER             "clang++")
