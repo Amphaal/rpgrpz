232b5ed2d4fb174eea68cb1ceb03ef4dad4d4883
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-13 14:42:35 +0200

    fdf

--- cmake/toolchains/linux-ci.cmake
+++ cmake/toolchains/linux-ci.cmake
@@ -1,7 +1,5 @@
 #We want Release with debug infos
-SET(CMAKE_BUILD_TYPE Release)
-
-SET(Qt5Multimedia_DIR   "usr/lib/cmake/Qt5Multimedia")
+SET (CMAKE_BUILD_TYPE Release)
 
 SET (CMAKE_C_COMPILER   "clang")
 SET (CMAKE_CXX_COMPILER "clang++")
