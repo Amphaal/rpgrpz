1183113f39078a4a7a2bcda9b27b7722e992f967
Amphaal <guillaume.vara@gmail.com>
2020-04-18 04:25:37 +0200

    linting

--- src/helpers/RPZQVariant.hpp
+++ src/helpers/RPZQVariant.hpp
@@ -27,149 +27,143 @@
 
 class RPZQVariant {
  public:
-        using AtomTransformationList = QHash<RPZAtom::Parameter, QVariant>;
+    using AtomTransformationList = QHash<RPZAtom::Parameter, QVariant>;
 
-        enum class Roles {
-            AtomId = 1006,
-            YTVideoMetadataPtr = 1007,
-            GridAlignable = 1100,
-            BrushTransform = 2555, 
-            IsTemporary = 2666, 
-            IsManuallyHidden = 2680, 
-            BrushDrawStyle = 2767,
-            CachedOpacity = 6000,
-            ContextuallyOwned = 7000,
-            AllowedToDefineMoveAbility = 8000,
-            CharacterId = 8010,
-            CoveredByFog = 9000,
-            FogSensitive = 9010,
-            GraphicsItemToReplace = 10100,
-            MoveAnimationDestinationScenePoint = 11000,
-            OverlappingGridBound = 12000
-        };
+    enum class Roles {
+        AtomId = 1006,
+        YTVideoMetadataPtr = 1007,
+        GridAlignable = 1100,
+        BrushTransform = 2555,
+        IsTemporary = 2666,
+        IsManuallyHidden = 2680,
+        BrushDrawStyle = 2767,
+        CachedOpacity = 6000,
+        ContextuallyOwned = 7000,
+        AllowedToDefineMoveAbility = 8000,
+        CharacterId = 8010,
+        CoveredByFog = 9000,
+        FogSensitive = 9010,
+        GraphicsItemToReplace = 10100,
+        MoveAnimationDestinationScenePoint = 11000,
+        OverlappingGridBound = 12000
+    };
 
-        static QVariant moveAnimationDestinationScenePoint(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint);
-        }
-        static void setMoveAnimationDestinationScenePoint(QGraphicsItem* item, const QPointF &sceneDestPoint) {
-            item->setData((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint, QVariant::fromValue<QPointF>(sceneDestPoint));
-        }
-        static void deleteMoveAnimationDestinationScenePoint(QGraphicsItem* item) {
-            item->setData((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint, QVariant());
-        }
+    static QVariant moveAnimationDestinationScenePoint(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint);
+    }
+    static void setMoveAnimationDestinationScenePoint(QGraphicsItem* item, const QPointF &sceneDestPoint) {
+        item->setData((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint, QVariant::fromValue<QPointF>(sceneDestPoint));
+    }
+    static void deleteMoveAnimationDestinationScenePoint(QGraphicsItem* item) {
+        item->setData((int)RPZQVariant::Roles::MoveAnimationDestinationScenePoint, QVariant());
+    }
 
-        static QGraphicsItem* graphicsItemToReplace(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::GraphicsItemToReplace).value<QGraphicsItem*>();
-        }
-        static void setGraphicsItemToReplace(QGraphicsItem* item, QGraphicsItem* toReplace) {
-            item->setData((int)RPZQVariant::Roles::GraphicsItemToReplace, QVariant::fromValue<QGraphicsItem*>(toReplace));
-        }
+    static QGraphicsItem* graphicsItemToReplace(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::GraphicsItemToReplace).value<QGraphicsItem*>();
+    }
+    static void setGraphicsItemToReplace(QGraphicsItem* item, QGraphicsItem* toReplace) {
+        item->setData((int)RPZQVariant::Roles::GraphicsItemToReplace, QVariant::fromValue<QGraphicsItem*>(toReplace));
+    }
 
-        static void setContextuallyOwned(QGraphicsItem* item, bool owned) {
-            item->setData((int)RPZQVariant::Roles::ContextuallyOwned, owned);
-        }
-        static bool contextuallyOwned(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::ContextuallyOwned).toBool();
-        }
+    static void setContextuallyOwned(QGraphicsItem* item, bool owned) {
+        item->setData((int)RPZQVariant::Roles::ContextuallyOwned, owned);
+    }
+    static bool contextuallyOwned(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::ContextuallyOwned).toBool();
+    }
 
-        static bool allowedToDefineMoveAbility(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::AllowedToDefineMoveAbility).toBool();
-        }
-        static void setAllowedToDefineMoveAbility(QGraphicsItem* item, bool allowed) {
-            item->setData((int)RPZQVariant::Roles::AllowedToDefineMoveAbility, allowed);
-        }
+    static bool allowedToDefineMoveAbility(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::AllowedToDefineMoveAbility).toBool();
+    }
+    static void setAllowedToDefineMoveAbility(QGraphicsItem* item, bool allowed) {
+        item->setData((int)RPZQVariant::Roles::AllowedToDefineMoveAbility, allowed);
+    }
 
-        static VideoMetadata* ytVideoMetadata(const QListWidgetItem* item) {
-            return item->data((int)RPZQVariant::Roles::YTVideoMetadataPtr).value<VideoMetadata*>();
-        }
-        static void setYTVideoMetadata(QListWidgetItem* item, VideoMetadata* metadata) {
-            item->setData((int)RPZQVariant::Roles::YTVideoMetadataPtr, QVariant::fromValue<VideoMetadata*>(metadata));
-        }
+    static VideoMetadata* ytVideoMetadata(const QListWidgetItem* item) {
+        return item->data((int)RPZQVariant::Roles::YTVideoMetadataPtr).value<VideoMetadata*>();
+    }
+    static void setYTVideoMetadata(QListWidgetItem* item, VideoMetadata* metadata) {
+        item->setData((int)RPZQVariant::Roles::YTVideoMetadataPtr, QVariant::fromValue<VideoMetadata*>(metadata));
+    }
 
-        static RPZAtom::Id atomId(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::AtomId).toULongLong();
-        }
-        static void setAtomId(QGraphicsItem* item, const RPZAtom::Id &id) {
-            item->setData((int)RPZQVariant::Roles::AtomId, QVariant::fromValue<RPZAtom::Id>(id));
-        }
+    static RPZAtom::Id atomId(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::AtomId).toULongLong();
+    }
+    static void setAtomId(QGraphicsItem* item, const RPZAtom::Id &id) {
+        item->setData((int)RPZQVariant::Roles::AtomId, QVariant::fromValue<RPZAtom::Id>(id));
+    }
 
-        static RPZCharacter::Id boundCharacterId(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::CharacterId).toULongLong();
-        }
-        static void setBoundCharacterId(QGraphicsItem* item, const RPZCharacter::Id &characterId) {
-            item->setData((int)RPZQVariant::Roles::CharacterId, QVariant::fromValue<RPZCharacter::Id>(characterId));
-        }
+    static RPZCharacter::Id boundCharacterId(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::CharacterId).toULongLong();
+    }
+    static void setBoundCharacterId(QGraphicsItem* item, const RPZCharacter::Id &characterId) {
+        item->setData((int)RPZQVariant::Roles::CharacterId, QVariant::fromValue<RPZCharacter::Id>(characterId));
+    }
 
-        static auto brushTransform(QGraphicsItem *item) {
-            
-            auto rawHash = item->data((int)RPZQVariant::Roles::BrushTransform).toHash();
-            
-            AtomTransformationList out;
-            
-            for(auto i = rawHash.begin(); i != rawHash.end(); i++) {
-                auto param = static_cast<RPZAtom::Parameter>(i.key().toInt());
-                out.insert(param, i.value());
-            }
-
-            return out;
-
-        }
-        static void setBrushTransform(QGraphicsItem *item, const AtomTransformationList &transforms) {
-
-            QVariantHash out;
-            for(auto i = transforms.begin(); i != transforms.end(); i++) {
-                auto toStr = QString::number((int)i.key());
-                out.insert(toStr, i.value());
-            }
-
-            item->setData((int)RPZQVariant::Roles::BrushTransform, out);
-            
+    static auto brushTransform(QGraphicsItem *item) {
+        
+        auto rawHash = item->data((int)RPZQVariant::Roles::BrushTransform).toHash();
+        
+        AtomTransformationList out;
+        
+        for(auto i = rawHash.begin(); i != rawHash.end(); i++) {
+            auto param = static_cast<RPZAtom::Parameter>(i.key().toInt());
+            out.insert(param, i.value());
         }
 
-        static bool isTemporary(QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::IsTemporary).toBool();
+        return out;
+    }
+    static void setBrushTransform(QGraphicsItem *item, const AtomTransformationList &transforms) {
+        QVariantHash out;
+        for (auto i = transforms.begin(); i != transforms.end(); i++) {
+            auto toStr = QString::number((int)i.key());
+            out.insert(toStr, i.value());
         }
-        static void setIsTemporary(QGraphicsItem* item, bool isTemporary) {
-            item->setData((int)RPZQVariant::Roles::IsTemporary, isTemporary);
-        }
+        item->setData((int)RPZQVariant::Roles::BrushTransform, out);
+    }
 
-        static bool isManuallyHidden(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::IsManuallyHidden).toBool();
-        }
-        static void setIsManuallyHidden(QGraphicsItem* item, bool isHidden) {
-            item->setData((int)RPZQVariant::Roles::IsManuallyHidden, isHidden);
-        }
+    static bool isTemporary(QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::IsTemporary).toBool();
+    }
+    static void setIsTemporary(QGraphicsItem* item, bool isTemporary) {
+        item->setData((int)RPZQVariant::Roles::IsTemporary, isTemporary);
+    }
 
-        static void setIsCoveredByFog(QGraphicsItem* item, bool isCovered) {
-            item->setData((int)RPZQVariant::Roles::CoveredByFog, isCovered);
-        }
-        static bool isCoveredByFog(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::CoveredByFog).toBool();
-        }
+    static bool isManuallyHidden(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::IsManuallyHidden).toBool();
+    }
+    static void setIsManuallyHidden(QGraphicsItem* item, bool isHidden) {
+        item->setData((int)RPZQVariant::Roles::IsManuallyHidden, isHidden);
+    }
 
-        static void setFogSensitive(QGraphicsItem* item, bool isFogSensitive) {
-            item->setData((int)RPZQVariant::Roles::FogSensitive, isFogSensitive);
-        }
-        static bool fogSensitive(const QGraphicsItem* item) {
-            return item->data((int)RPZQVariant::Roles::FogSensitive).toBool();
-        }
+    static void setIsCoveredByFog(QGraphicsItem* item, bool isCovered) {
+        item->setData((int)RPZQVariant::Roles::CoveredByFog, isCovered);
+    }
+    static bool isCoveredByFog(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::CoveredByFog).toBool();
+    }
 
-        static double cachedOpacity(QGraphicsItem* item) {
-            bool ok = false;
-            auto data = item->data((int)RPZQVariant::Roles::CachedOpacity).toDouble(&ok);
-            return ok ? data : 1;
-        }
-        static void setCachedOpacity(QGraphicsItem* item, double cached) {
-            item->setData((int)RPZQVariant::Roles::CachedOpacity, cached);
-        }
+    static void setFogSensitive(QGraphicsItem* item, bool isFogSensitive) {
+        item->setData((int)RPZQVariant::Roles::FogSensitive, isFogSensitive);
+    }
+    static bool fogSensitive(const QGraphicsItem* item) {
+        return item->data((int)RPZQVariant::Roles::FogSensitive).toBool();
+    }
 
-        static RPZAtom::BrushType brushDrawStyle(QGraphicsItem* item) {
-            return (RPZAtom::BrushType)item->data((int)RPZQVariant::Roles::BrushDrawStyle).toInt();
-        }
-        static void setBrushDrawStyle(QGraphicsItem* item, const RPZAtom::BrushType &style) {
-            item->setData((int)RPZQVariant::Roles::BrushDrawStyle, (int)style);
-        }
+    static double cachedOpacity(QGraphicsItem* item) {
+        bool ok = false;
+        auto data = item->data((int)RPZQVariant::Roles::CachedOpacity).toDouble(&ok);
+        return ok ? data : 1;
+    }
+    static void setCachedOpacity(QGraphicsItem* item, double cached) {
+        item->setData((int)RPZQVariant::Roles::CachedOpacity, cached);
+    }
 
-
+    static RPZAtom::BrushType brushDrawStyle(QGraphicsItem* item) {
+        return (RPZAtom::BrushType)item->data((int)RPZQVariant::Roles::BrushDrawStyle).toInt();
+    }
+    static void setBrushDrawStyle(QGraphicsItem* item, const RPZAtom::BrushType &style) {
+        item->setData((int)RPZQVariant::Roles::BrushDrawStyle, (int)style);
+    }
 };
 inline uint qHash(const RPZQVariant::Roles &key, uint seed = 0) {return uint(key) ^ seed;}
--- src/helpers/VectorSimplifier.hpp
+++ src/helpers/VectorSimplifier.hpp
@@ -28,196 +28,173 @@
 
 class VectorSimplifier {
  public:
-        static QVector<QPointF> reduce(QVector<QPointF> points, double tolerance = 1.0, bool highestQuality = true) {
-            
-            if (points.count() <= 2) return points;
+    static QVector<QPointF> reduce(QVector<QPointF> points, double tolerance = 1.0, bool highestQuality = true) {
+        if (points.count() <= 2) return points;
 
-            auto sqTolerance = tolerance * tolerance;
+        auto sqTolerance = tolerance * tolerance;
 
-            points = highestQuality ? points : _reduceRadialDist(points, sqTolerance);
-            points = _reduceDouglasPeucker(points, sqTolerance);
+        points = highestQuality ? points : _reduceRadialDist(points, sqTolerance);
+        points = _reduceDouglasPeucker(points, sqTolerance);
 
-            return points;
+        return points;
+    }
 
+    static QList<QPolygonF> simplifyPolygon(const QPolygonF &poly) {
+        ClipperLib::Paths outRaw;
+        ClipperLib::SimplifyPolygon(_toPath(poly), outRaw, ClipperLib::PolyFillType::pftNonZero);
+        return toPolys(outRaw);
+    }
+
+    static ClipperLib::Paths toPaths(const QList<QPolygonF> &sourcePolys) {
+        ClipperLib::Paths out;
+        for (const auto &poly : sourcePolys) {
+            out.push_back(_toPath(poly));
         }
+        return out;
+    }
 
-        static QList<QPolygonF> simplifyPolygon(const QPolygonF &poly) {
-            ClipperLib::Paths outRaw;
-            ClipperLib::SimplifyPolygon(_toPath(poly), outRaw, ClipperLib::PolyFillType::pftNonZero);
-            return toPolys(outRaw);
-        }
+    static QList<QPolygonF> toPolys(const ClipperLib::Paths &paths) {
+        QList<QPolygonF> out;
 
-        static ClipperLib::Paths toPaths(const QList<QPolygonF> &sourcePolys) {
-            ClipperLib::Paths out;
-            for(const auto &poly : sourcePolys) {
-                out.push_back(_toPath(poly));
-            }
-            return out;
-        }
+        for (const auto &path : paths) {
+            QPolygonF poly;
 
-        static QList<QPolygonF> toPolys(const ClipperLib::Paths &paths) {
-            
-            QList<QPolygonF> out;
-            
-            for(const auto &path : paths) {
-                
-                QPolygonF poly;
-
-                for(const auto &point : path) {
-                    poly << QPointF(
-                        ((double)point.X) / _precision, 
-                        ((double)point.Y) / _precision 
-                    );
-                }
-
-                out << poly;
-
+            for (const auto &point : path) {
+                poly << QPointF(
+                    ((double)point.X) / _precision,
+                    ((double)point.Y) / _precision);
             }
 
-            return out;
-
+            out << poly;
         }
 
-        static const QPolygonF reducePolygon(const QPolygonF &toreduce) {
-            return reduce(toreduce);
-        } 
+        return out;
+    }
 
-        static const QPainterPath reducePath(const QPainterPath &sourcePath) {
-            
-            //condense coords
-            QVector<QPointF> toReduce;
-            
-            for(auto i = 0; i < sourcePath.elementCount(); i++) {
-                
-                auto elem = sourcePath.elementAt(i);
-                if(!elem.isLineTo()) continue;
+    static const QPolygonF reducePolygon(const QPolygonF &toreduce) {
+        return reduce(toreduce);
+    }
 
-                toReduce.push_back({ elem.x, elem.y });
+    static const QPainterPath reducePath(const QPainterPath &sourcePath) {
+        // condense coords
+        QVector<QPointF> toReduce;
 
+        for (auto i = 0; i < sourcePath.elementCount(); i++) {
+            auto elem = sourcePath.elementAt(i);
+            if (!elem.isLineTo()) continue;
+
+            toReduce.push_back({ elem.x, elem.y });
+        }
+
+        // reduce
+        auto simplified = reduce(toReduce);
+
+        QPainterPath destPath;
+        destPath.moveTo(0, 0);
+
+        // fill painterpath
+        for (auto &point : simplified) {
+            destPath.lineTo(point);
+        }
+
+        return destPath;
+    }
+
+ private:
+    static constexpr int _precision = 3;
+
+    static ClipperLib::Path _toPath(const QPolygonF &sourcePoly) {
+        ClipperLib::Path rawPath;
+
+        for (const auto &point : sourcePoly) {
+            rawPath.push_back({ 
+                (int)(point.x() * _precision), 
+                (int)(point.y()  * _precision)
+            });
+        }
+
+        return rawPath;
+    }
+
+    // square distance between 2 points
+    static double _getSqDist(const QPointF &p1, const QPointF &p2) {
+        auto dx = p1.x() - p2.x();
+        auto dy = p1.y() - p2.y();
+
+        return dx * dx + dy * dy;
+    }
+
+    // square distance from a point to a segment
+    static double _getSqSegDist(const QPointF &p, const QPointF &p1, const QPointF &p2) {
+        auto x = p1.x();
+        auto y = p1.y();
+        auto dx = p2.x() - x;
+        auto dy = p2.y() - y;
+
+        if (dx != 0 || dy != 0) {
+            auto t = ((p.x() - x) * dx + (p.y() - y) * dy) / (dx * dx + dy * dy);
+
+            if (t > 1) {
+                x = p2.x();
+                y = p2.y();
+
+            } else if (t > 0) {
+                x += dx * t;
+                y += dy * t;
             }
+        }
 
-            //reduce
-            auto simplified = reduce(toReduce);
-            
-            QPainterPath destPath;
-            destPath.moveTo(0,0);
+        dx = p.x() - x;
+        dy = p.y() - y;
 
-            //fill painterpath
-            for(auto &point : simplified) {
-                destPath.lineTo(point);
+        return dx * dx + dy * dy;
+    }
+
+    // basic distance-based simplification
+    static QVector<QPointF> _reduceRadialDist(const QVector<QPointF> &points, double sqTolerance) {
+        auto prevPoint = points.first();
+        QVector<QPointF> newPoints {prevPoint};
+        QPointF point;
+
+        for (auto i = 1; i < points.count(); i++) {
+            point = points.at(i);
+
+            if (_getSqDist(point, prevPoint) > sqTolerance) {
+                newPoints.append(point);
+                prevPoint = point;
             }
-
-            return destPath;
-
         }
 
- private:  
-        static constexpr int _precision = 3;
+        if (prevPoint != point) newPoints.append(point);
 
-        static ClipperLib::Path _toPath(const QPolygonF &sourcePoly) {
-            
-            ClipperLib::Path rawPath;
-            
-            for(const auto &point : sourcePoly) {
-                
-                rawPath.push_back({ 
-                    (int)(point.x() * _precision), 
-                    (int)(point.y()  * _precision)
-                });
+        return newPoints;
+    }
 
+    static void _reduceDPStep(const QVector<QPointF> &points, int first, int last, double sqTolerance, QVector<QPointF> &simplified) {
+        auto maxSqDist = sqTolerance;
+        int index = 0;
+
+        for (auto i = first + 1; i < last; i++) {
+            auto sqDist = _getSqSegDist(points.at(i), points.at(first), points.at(last));
+
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
             }
-
-            return rawPath;
-
         }
 
-        // square distance between 2 points
-        static double _getSqDist(const QPointF &p1, const QPointF &p2) {
-
-            auto dx = p1.x() - p2.x();
-            auto dy = p1.y() - p2.y();
-
-            return dx * dx + dy * dy;
-
+        if (maxSqDist > sqTolerance) {
+            if (index - first > 1) _reduceDPStep(points, first, index, sqTolerance, simplified);
+            simplified.append(points.at(index));
+            if (last - index > 1) _reduceDPStep(points, index, last, sqTolerance, simplified);
         }
+    }
 
-        // square distance from a point to a segment
-        static double _getSqSegDist(const QPointF &p, const QPointF &p1, const QPointF &p2) {
-
-            auto x = p1.x();
-            auto y = p1.y();
-            auto dx = p2.x() - x;
-            auto dy = p2.y() - y;
-
-            if (dx != 0 || dy != 0) {
-
-                auto t = ((p.x() - x) * dx + (p.y() - y) * dy) / (dx * dx + dy * dy);
-
-                if (t > 1) {
-                    x = p2.x();
-                    y = p2.y();
-
-                } else if (t > 0) {
-                    x += dx * t;
-                    y += dy * t;
-                }
-            }
-
-            dx = p.x() - x;
-            dy = p.y() - y;
-
-            return dx * dx + dy * dy;
-        }
-
-        // basic distance-based simplification
-        static QVector<QPointF> _reduceRadialDist(const QVector<QPointF> &points, double sqTolerance) {
-
-            auto prevPoint = points.first();
-            QVector<QPointF> newPoints {prevPoint};
-            QPointF point;
-
-            for (auto i = 1; i < points.count(); i++) {
-                point = points.at(i);
-
-                if (_getSqDist(point, prevPoint) > sqTolerance) {
-                    newPoints.append(point);
-                    prevPoint = point;
-                }
-            }
-
-            if (prevPoint != point) newPoints.append(point);
-
-            return newPoints;
-
-        }
-
-        static void _reduceDPStep(const QVector<QPointF> &points, int first, int last, double sqTolerance, QVector<QPointF> &simplified) {
-            auto maxSqDist = sqTolerance;
-            int index = 0;
-
-            for (auto i = first + 1; i < last; i++) {
-                auto sqDist = _getSqSegDist(points.at(i), points.at(first), points.at(last));
-
-                if (sqDist > maxSqDist) {
-                    index = i;
-                    maxSqDist = sqDist;
-                }
-            }
-
-            if (maxSqDist > sqTolerance) {
-                if (index - first > 1) _reduceDPStep(points, first, index, sqTolerance, simplified);
-                simplified.append(points.at(index));
-                if (last - index > 1) _reduceDPStep(points, index, last, sqTolerance, simplified);
-            }
-            
-        }
-
-        // simplification using Ramer-Douglas-Peucker algorithm
-        static QVector<QPointF> _reduceDouglasPeucker(const QVector<QPointF> &points, double sqTolerance) {
-            QVector<QPointF> simplified { points.at(0) };
-            _reduceDPStep(points, 0, points.count() - 1, sqTolerance, simplified);
-            simplified += points.last();
-            return simplified;
-        }
-
+    // simplification using Ramer-Douglas-Peucker algorithm
+    static QVector<QPointF> _reduceDouglasPeucker(const QVector<QPointF> &points, double sqTolerance) {
+        QVector<QPointF> simplified { points.at(0) };
+        _reduceDPStep(points, 0, points.count() - 1, sqTolerance, simplified);
+        simplified += points.last();
+        return simplified;
+    }
 };
