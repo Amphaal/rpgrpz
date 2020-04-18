6826d465f26ce1e864740f28d70e90305f1794e7
Guillaume VARA <guillaume.vara@gmail.com>
2020-04-19 01:43:57 +0200

    linting

--- src/ui/atomEditor/AtomEditionManager.hpp
+++ src/ui/atomEditor/AtomEditionManager.hpp
@@ -27,131 +27,111 @@
 #include "src/shared/hints/HintThread.hpp"
 
 class AtomEditionManager : public QWidget {
-
     Q_OBJECT
 
  private:
-        AtomEditor* _editor = nullptr;
-        QPushButton* _resetButton = nullptr;
-        HiddenCheckbox* _defaultHiddenCheckbox = nullptr;
-        LayerSelector* _layerSelector = nullptr;
+    AtomEditor* _editor = nullptr;
+    QPushButton* _resetButton = nullptr;
+    HiddenCheckbox* _defaultHiddenCheckbox = nullptr;
+    LayerSelector* _layerSelector = nullptr;
 
-        void _handleSubjectChange(const AtomsSelectionDescriptor &atomsSelectDescriptor) {
-            this->_resetButton->setEnabled(false);
-            this->_editor->buildEditor(atomsSelectDescriptor);
-            this->_resetButton->setEnabled(
-                this->_editor->hasVisibleEditors()
-            );
-        }
+    void _handleSubjectChange(const AtomsSelectionDescriptor &atomsSelectDescriptor) {
+        this->_resetButton->setEnabled(false);
+        this->_editor->buildEditor(atomsSelectDescriptor);
+        this->_resetButton->setEnabled(
+            this->_editor->hasVisibleEditors()
+        );
+    }
 
  public:
-        AtomEditionManager(QWidget *parent = nullptr) : QWidget(parent), 
-            _editor(new AtomEditor), 
-            _resetButton(new QPushButton) {
-            
-            auto defaultGrpBox = new QGroupBox(QObject::tr("Default configuration"));
-            defaultGrpBox->setAlignment(Qt::AlignHCenter);
-            auto defaultLayout = new QHBoxLayout;
-            defaultGrpBox->setLayout(defaultLayout);
-            defaultLayout->setContentsMargins(10, 0, 0, 0);
-            this->_layerSelector = new LayerSelector(this);
-            this->_defaultHiddenCheckbox = new HiddenCheckbox(this);
-            defaultLayout->addWidget(this->_layerSelector);
-            defaultLayout->addStretch(1);
-            defaultLayout->addWidget(this->_defaultHiddenCheckbox);
+    explicit AtomEditionManager(QWidget *parent = nullptr) : QWidget(parent), _editor(new AtomEditor), _resetButton(new QPushButton) {
+        // setup
+        auto defaultGrpBox = new QGroupBox(QObject::tr("Default configuration"));
+        defaultGrpBox->setAlignment(Qt::AlignHCenter);
+        auto defaultLayout = new QHBoxLayout;
+        defaultGrpBox->setLayout(defaultLayout);
+        defaultLayout->setContentsMargins(10, 0, 0, 0);
+        this->_layerSelector = new LayerSelector(this);
+        this->_defaultHiddenCheckbox = new HiddenCheckbox(this);
+        defaultLayout->addWidget(this->_layerSelector);
+        defaultLayout->addStretch(1);
+        defaultLayout->addWidget(this->_defaultHiddenCheckbox);
 
-            this->_resetButton->setText(tr("Reset displayed parameters"));
+        this->_resetButton->setText(tr("Reset displayed parameters"));
 
-            auto layout = new QVBoxLayout;
-            this->setLayout(layout);
-            
-            layout->addWidget(defaultGrpBox, 0);
-            layout->addWidget(this->_editor, 1);
-            layout->addWidget(this->_resetButton);
+        auto layout = new QVBoxLayout;
+        this->setLayout(layout);
 
-            QObject::connect(
-                AlterationHandler::get(), &AlterationHandler::requiresPayloadHandling,
-                this, &AtomEditionManager::_handleAlterationRequest
-            );
+        layout->addWidget(defaultGrpBox, 0);
+        layout->addWidget(this->_editor, 1);
+        layout->addWidget(this->_resetButton);
 
-            QObject::connect(
-                this->_resetButton, &QPushButton::pressed,
-                this->_editor, &AtomEditor::resetParams
-            );
+        QObject::connect(
+            AlterationHandler::get(), &AlterationHandler::requiresPayloadHandling,
+            this, &AtomEditionManager::_handleAlterationRequest
+        );
 
-            this->_resetButton->setEnabled(false);
-        }
+        QObject::connect(
+            this->_resetButton, &QPushButton::pressed,
+            this->_editor, &AtomEditor::resetParams
+        );
 
-        AtomEditor* editor() {
-            return this->_editor;
-        }
+        this->_resetButton->setEnabled(false);
+    }
 
-        LayerSelector* layerSelector(){
-            return this->_layerSelector;
-        }
+    AtomEditor* editor() {
+        return this->_editor;
+    }
 
-        HiddenCheckbox* hiddenCheckbox() {
-            return this->_defaultHiddenCheckbox;
-        }
-    
+    LayerSelector* layerSelector() {
+        return this->_layerSelector;
+    }
+
+    HiddenCheckbox* hiddenCheckbox() {
+        return this->_defaultHiddenCheckbox;
+    }
+
  private slots:
-        void _handleAlterationRequest(const AlterationPayload &payload) {
-            
-            this->_editor->payloadTrace(payload);
-            
-            auto casted = Payloads::autoCast(payload);
+    void _handleAlterationRequest(const AlterationPayload &payload) {
+        this->_editor->payloadTrace(payload);
 
-            //if ghost item selection occured
-            if(auto mPayload = dynamic_cast<AtomTemplateSelectedPayload*>(casted.data())) {
-                
-                auto atom_template = mPayload->selectedTemplate();
-                AtomsSelectionDescriptor descr;
-                
-                if(!atom_template.isEmpty()) {
-                    
-                    auto type = atom_template.type();
-                    descr.representedTypes.insert(type);
+        auto casted = Payloads::autoCast(payload);
 
-                    descr.templateAtom = atom_template;
+        // if ghost item selection occured
+        if (auto mPayload = dynamic_cast<AtomTemplateSelectedPayload*>(casted.data())) {
+            auto atom_template = mPayload->selectedTemplate();
+            AtomsSelectionDescriptor descr;
 
-                }
+            if (!atom_template.isEmpty()) {
+                auto type = atom_template.type();
+                descr.representedTypes.insert(type);
 
-                this->_handleSubjectChange(descr);
-                
+                descr.templateAtom = atom_template;
             }
 
-            //if selection occured
-            else if(auto mPayload = dynamic_cast<SelectedPayload*>(casted.data())) {
-                auto selected = mPayload->targetRPZAtomIds();
-                auto descr = HintThread::hint()->getAtomSelectionDescriptor(selected);
-                this->_handleSubjectChange(descr);
-            }
+            this->_handleSubjectChange(descr);
+        } else if (auto mPayload = dynamic_cast<SelectedPayload*>(casted.data())) {  // if selection occured
+            auto selected = mPayload->targetRPZAtomIds();
+            auto descr = HintThread::hint()->getAtomSelectionDescriptor(selected);
+            this->_handleSubjectChange(descr);
+        } else if (auto mPayload = dynamic_cast<RemovedPayload*>(casted.data())) {  // if deletion happened
+            auto currSelectionDescr = this->_editor->currentSelectionDescriptor();
 
-            //if deletion happened
-            else if(auto mPayload = dynamic_cast<RemovedPayload*>(casted.data())) {
-                
-                auto currSelectionDescr = this->_editor->currentSelectionDescriptor();
+            auto selected = mPayload->targetRPZAtomIds();
 
-                auto selected = mPayload->targetRPZAtomIds();
-                
-                auto removed = QSet<RPZAtom::Id>(selected.begin(), selected.end());
-                auto current = QSet<RPZAtom::Id>(currSelectionDescr.selectedAtomIds.begin(), currSelectionDescr.selectedAtomIds.end());
-                
-                //if no previous selection and has template atom, keep current description
-                if(!currSelectionDescr.templateAtom.isEmpty() && !current.count()) return;
+            auto removed = QSet<RPZAtom::Id>(selected.begin(), selected.end());
+            auto current = QSet<RPZAtom::Id>(currSelectionDescr.selectedAtomIds.begin(), currSelectionDescr.selectedAtomIds.end());
 
-                auto truncated = current.subtract(removed).values();
-                auto descr = HintThread::hint()->getAtomSelectionDescriptor(truncated);
+            // if no previous selection and has template atom, keep current description
+            if (!currSelectionDescr.templateAtom.isEmpty() && !current.count()) return;
 
-                this->_handleSubjectChange(descr);
-            }
-            
-            //on reset
-            else if(auto mPayload = dynamic_cast<RemovedPayload*>(casted.data())) {
-                AtomsSelectionDescriptor descr;
-                this->_handleSubjectChange(descr);
-            }
+            auto truncated = current.subtract(removed).values();
+            auto descr = HintThread::hint()->getAtomSelectionDescriptor(truncated);
 
+            this->_handleSubjectChange(descr);
+        } else if (auto mPayload = dynamic_cast<RemovedPayload*>(casted.data())) {  // on reset
+            AtomsSelectionDescriptor descr;
+            this->_handleSubjectChange(descr);
         }
-
+    }
 };
