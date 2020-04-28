791ff7fe48822810b2292e4cb883e7b1da3fda05
Amphaal <guillaume.vara@gmail.com>
2020-04-28 05:23:56 +0200

    fix local macos build

--- cmake/toolchains/osx.cmake
+++ cmake/toolchains/osx.cmake
@@ -1,14 +1,10 @@
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON) #prevent linking errors once app shared
-SET(CMAKE_MACOSX_RPATH ON)
-
-#set(CMAKE_OSX_SYSROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk)
 SET(CMAKE_OSX_DEPLOYMENT_TARGET 10.13)
 
 list(APPEND CMAKE_PREFIX_PATH 
     "/usr/local/opt"
     "/usr/local/opt/qt"
+    "/usr/local/opt/glib-networking"
+    "/usr/local/opt/openssl@1.1"
 )
-
-set(OPENSSL_ROOT_DIR "/usr/local/Cellar/openssl@1.1/1.1.1f")
 
 SET(CMAKE_BUILD_TYPE Debug)
