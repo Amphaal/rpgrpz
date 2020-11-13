7e2066bb15fd25a9ba31624f5450600b4ac1ecf3
Amphaal <guillaume.vara@gmail.com>
2020-11-13 01:05:43 +0100

    Update windows-ci.cmake

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -3,7 +3,7 @@ SET(CMAKE_SYSTEM_VERSION 6.1 CACHE INTERNAL "") #target Windows 7
 SET(MINGW ON)
 
 # search for programs in the build host directories
-set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # for libraries and headers in the target directories
 set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
